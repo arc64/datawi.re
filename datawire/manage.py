ddce385c22284ec68797b512fade8599c76ce3d1
Friedrich Lindenberg <friedrich@pudo.org>
2013-05-16 22:35:16 +0200

    Create an admin user on db initialisation.

--- datawire/manage.py
+++ datawire/manage.py
@@ -1,16 +1,20 @@
 from flask.ext.script import Manager
 
 from datawire.core import app, db
-from datawire.model import Service
+from datawire.model import User
 from datawire.views import index
 
 manager = Manager(app)
 
 
 @manager.command
-def create_db():
+def createdb():
     """ Create the database entities. """
     db.create_all()
+    admin_data = {'screen_name': 'admin', 'display_name': 'Systems Admin'}
+    if User.by_screen_name(admin_data.get('screen_name')) is None:
+        admin = User.create(admin_data)
+    db.session.commit() 
 
 
 if __name__ == "__main__":
