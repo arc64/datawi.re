5292c22aa7240f2a756de8bbbeaf63f3f7f50240
Friedrich Lindenberg <friedrich@pudo.org>
2013-05-16 22:34:16 +0200

    Add common retrieval methods.

--- datawire/model/util.py
+++ datawire/model/util.py
@@ -11,6 +11,15 @@ class ModelCore(object):
     updated_at = db.Column(db.DateTime, default=datetime.utcnow,
                            onupdate=datetime.utcnow)
 
+    @classmethod
+    def by_id(cls, id):
+        q = db.session.query(cls).filter_by(id=id)
+        return q.first()
+
+    @classmethod
+    def all(cls):
+        return db.session.query(cls)
+
 
 def make_token():
     return uuid.uuid4().get_hex()[15:]
