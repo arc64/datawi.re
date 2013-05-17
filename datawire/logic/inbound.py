e923d1f1fc07cb9edd9c3eeed55b91beda07d014
Friedrich Lindenberg <friedrich@pudo.org>
2013-05-17 19:44:26 +0200

    Bring it together and actually make frames into persistent objects.

--- datawire/logic/inbound.py
+++ datawire/logic/inbound.py
@@ -1,12 +1,13 @@
 from pprint import pprint
 from datetime import datetime
 
-from datawire.exc import NotFound
+from datawire.exc import NotFound, BadRequest
+from datawire.core import db
 from datawire.model import Service, Event, Frame
 from datawire.model.util import make_token, data_hash
+from datawire.store import store_frame
 
-
-def handle_frame(service_key, event_key, data):
+def generate_frame(service_key, event_key, data):
     service = Service.by_key(service_key)
     if service is None:
         raise NotFound('No such service: %s' % service_key)
@@ -15,13 +16,21 @@ def handle_frame(service_key, event_key, data):
         raise NotFound('No such event: %s' % event_key)
 
     frame = {
-        'id': make_token(),
         'service': service_key,
         'event': event_key,
-        'hash': data_hash(data),
-        'created_at': datetime.utcnow(),
         'data': data
     }
+    frame['hash'] = data_hash(frame)
+    frame.update({
+        'id': make_token(),
+        'created_at': datetime.utcnow()
+    })
     frame['urn'] = Frame.to_urn(frame)
 
+    if Frame.by_hash(frame['hash']) is not None:
+        raise BadRequest('Duplicate content, hash: %(hash)s' % frame)
+
+    Frame.create(service, frame)
+    store_frame(frame)
+    db.session.commit()
     pprint(frame)
--- datawire/views/inbound.py
+++ datawire/views/inbound.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request
 
 from datawire.exc import BadRequest
 from datawire.views.util import jsonify
-from datawire.logic.inbound import handle_frame
+from datawire.logic.inbound import generate_frame
 
 inbound = Blueprint('inbound', __name__)
 
@@ -12,5 +12,5 @@ inbound = Blueprint('inbound', __name__)
 def submit(service_key, event_key):
     if request.json is None:
         raise BadRequest('Data must be submitted as JSON.')
-    handle_frame(service_key, event_key, request.json)
+    generate_frame(service_key, event_key, request.json)
     return jsonify({'status': 'ok'})
