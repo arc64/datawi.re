8c813f86bcc3143eed55a5152807a3574894223f
Friedrich Lindenberg <friedrich@pudo.org>
2013-05-17 19:44:07 +0200

    Create method for frames.

--- datawire/model/frame.py
+++ datawire/model/frame.py
@@ -7,11 +7,27 @@ class Frame(db.Model):
     __tablename__ = 'frame'
 
     urn = db.Column(db.Unicode(), primary_key=True)
-    hash = db.Column(db.Unicode())
+    hash = db.Column(db.Unicode(), index=True)
     service_id = db.Column(db.Integer(), db.ForeignKey('service.id'))
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime)
+
+    @classmethod
+    def create(cls, service, data):
+        # TODO: ensure this is the only event on this service with
+        # this key.
+        obj = cls()
+        obj.urn = data.get('urn')
+        obj.hash = data.get('hash')
+        obj.created_at = data.get('created_at')
+        obj.service = service
+        db.session.add(obj)
+        return obj
 
     @classmethod
     def to_urn(cls, frame):
         return 'urn:dwre:%(service)s:%(event)s:%(id)s' % frame
 
+    @classmethod
+    def by_hash(cls, hash):
+        q = db.session.query(cls).filter_by(hash=hash)
+        return q.first()
