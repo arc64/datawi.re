f7cb7dfd6849533d1b62e9d5def9492d5c00c61b
Friedrich Lindenberg <friedrich@pudo.org>
2013-05-18 13:41:26 +0200

    API to download frame data.

--- datawire/views/frames.py
+++ datawire/views/frames.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 
-from datawire.exc import BadRequest
+from datawire.exc import BadRequest, NotFound
+from datawire.store import load_frame
 from datawire.views.util import jsonify
 from datawire.logic.inbound import generate_frame
 
@@ -14,3 +15,12 @@ def submit(service_key, event_key):
         raise BadRequest('Data must be submitted as JSON.')
     generate_frame(service_key, event_key, request.json)
     return jsonify({'status': 'ok'})
+
+
+@frames.route('/frames/<urn>')
+def get(urn):
+    # TODO: Cache headers.
+    data = load_frame(urn)
+    if data is None:
+        raise NotFound('Frame: %s' % urn)
+    return jsonify(data)
