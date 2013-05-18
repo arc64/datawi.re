d40fdb8c1ac7ddfd55c029cf2c0da1f3f6dba173
Friedrich Lindenberg <friedrich@pudo.org>
2013-05-18 14:16:21 +0200

    begin laying out the authorisation layer.

---
+++ datawire/auth/__init__.py
@@ -0,0 +1,34 @@
+import inspect
+from flask import request
+
+from datahub.exc import Forbidden
+import services
+
+
+def logged_in():
+    return request.user is None
+
+
+class Requirement(object):
+    """ Checks a function call and raises an exception if the
+    function returns a non-True value. """
+
+    def __init__(self, wrapped):
+        self.wrapped = wrapped
+
+    def __getattr__(self, attr):
+        real = getattr(self.wrapped, attr)
+        return Requirement(real)
+
+    def __call__(self, *args, **kwargs):
+        fc = self.wrapped(*args, **kwargs)
+        if fc is not True:
+            raise Forbidden('Sorry, you\'re not permitted to do this.')
+        return fc
+
+    @classmethod
+    def here(cls):
+        module = inspect.getmodule(cls)
+        return cls(module)
+
+require = Requirement.here()
---
+++ datawire/auth/services.py
@@ -0,0 +1,17 @@
+from flask import request
+
+
+def create():
+    pass
+
+def view(service):
+    pass
+
+def admin(service):
+    pass
+
+def publish(service):
+    pass
+
+def delete(service):
+    pass
