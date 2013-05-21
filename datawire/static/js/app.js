299750e6f9cccda3d1cfb4c7298e2a761a66c1cd
Friedrich Lindenberg <friedrich@pudo.org>
2013-05-21 23:26:20 +0200

    Profile saving front-end.

--- datawire/static/js/app.js
+++ datawire/static/js/app.js
@@ -1,5 +1,5 @@
 
-var module = angular.module('datawire', [], function($routeProvider, $locationProvider) {
+var datawire = angular.module('datawire', [], function($routeProvider, $locationProvider) {
   $routeProvider.when('/profile', {
     templateUrl: '/static/partials/profile.html',
     controller: ProfileCntl
@@ -13,19 +13,56 @@ var module = angular.module('datawire', [], function($routeProvider, $locationPr
   $locationProvider.html5Mode(true);
 });
 
-module.run(function($rootScope) {
+datawire.run(function($rootScope) {
     $rootScope.visit = function(url) {
         $window.location.href = url;
     };
+
+    $rootScope.tableObject = function(obj) {
+        var table = {};
+        angular.forEach(obj, function(v, k) {
+            if (v && v.length) {
+                table[k] = v;
+            }
+        });
+        return table;
+    };
+
+    $rootScope.flash = function(type, message) {
+        $rootScope.currentFlash = {
+            visible: true,
+            type: type,
+            message: message
+        };
+    };
 });
 
-function ProfileCntl($scope, $routeParams) {
-  $scope.message = 'I am a banana!';
+datawire.factory('identity', function($http) {
+    var dfd = $http.get('/api/1/sessions');
+    return {
+        session: dfd.success
+    };
+});
+
+function ProfileCntl($scope, $routeParams, $http) {
+    $http.get('/api/1/profile').success(function(data) {
+        $scope.profile = data;
+    });
+
+    $scope.save = function() {
+        var dfd = $http.post('/api/1/profile', $scope.profile);
+        dfd.success(function(data) {
+            $scope.profile = data;
+            $scope.flash('success', 'Your profile has been updated.');
+        });
+        dfd.error(function(data) {
+            $scope.flash('error', 'There was an error saving your profile.');
+        });
+    };
 }
 
-function NavigationCntl($scope, $window, $routeParams, $http) {
-    $http.get('/api/1/sessions?limit=20').success(function(data) {
-        console.log(data);
+function NavigationCntl($scope, $window, $routeParams, identity) {
+    identity.session(function(data) {
         $scope.session = data;
     });
     $scope.visit = function(url) {
@@ -34,7 +71,7 @@ function NavigationCntl($scope, $window, $routeParams, $http) {
 }
 
 function FeedCntl($scope, $routeParams, $http) {
-    $http.get('/api/1/frames').success(function(data) {
+    $http.get('/api/1/frames?limit=20').success(function(data) {
         console.log(data);
         $scope.frames = data.results;
         $scope.services = data.services;
