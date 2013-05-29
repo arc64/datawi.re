ccbfa45918b7c234046c4f9376796b5c434259a1
Friedrich Lindenberg <friedrich@pudo.org>
2013-05-29 12:07:27 +0200

    Fix up app.'s and extract a feed service.

--- datawire/static/js/app.js
+++ datawire/static/js/app.js
@@ -41,54 +41,25 @@ Handlebars.registerHelper('entity', function(text) {
     return new Handlebars.SafeString(result);
 });
 
-datawire.factory('identity', function($http) {
-    var dfd = $http.get('/api/1/sessions');
-    return {
-        session: dfd.success,
-        checkSession: function(callback) {
-            dfd.success(function(data) {
-                if (!data.logged_in) {
-                    callback(data);
-                }
-            });
-        }
-    };
-});
+datawire.factory('feed', function($http, identity, services) {
 
-datawire.factory('services', function($q, $http) {
-    var serviceDfds = {};
-
-    function getService(name, callback) {
-        if(!serviceDfds[name]) {
-            serviceDfds[name] = $http.get('/api/1/services/' + name);
-        }
-        serviceDfds[name].success(callback);
-        return serviceDfds[name];
-    }
-
-    function getFrame(ref) {
-        $http.get(ref.store_uri).success(function(frame) {
-            getService(frame.service, function(service) {
-                ref.event = _.find(service.events, function(e) {
-                    return e.key == frame.event;
+    function update(callback) {
+        identity.session(function(ident) {
+            $http.get('/api/1/users/' + ident.user.id + '/feed').success(function(data) {
+                angular.forEach(data.results, function(frame, i) {
+                    services.getFrame(frame);
                 });
-                if(ref.event.tmpl===undefined) {
-                    ref.event.tmpl = Handlebars.compile(ref.event.template);
-                }
-                ref.service = service;
-                ref.data = frame.data;
-                ref.message = ref.event.tmpl(frame.data);
+                callback(data);
             });
         });
     }
 
     return {
-        getService: getService,
-        getFrame: getFrame
+        update: update
     };
 });
 
-function FeedCtrl($scope, $routeParams, $http, services) {
+function FeedCtrl($scope, $routeParams, feed) {
     $scope.tableObject = function(obj) {
         var table = {};
         angular.forEach(obj, function(v, k) {
@@ -99,53 +70,8 @@ function FeedCtrl($scope, $routeParams, $http, services) {
         return table;
     };
 
-    $http.get('/api/1/frames?limit=20').success(function(data) {
+    feed.update(function(data) {
         $scope.frames = data.results;
-        angular.forEach($scope.frames, function(frame, i) {
-            services.getFrame(frame);
-        });
-    });
-}
-
-function EntityCtrl($scope, $routeParams, $http, identity) {
-    $scope._new = {};
-    $scope.entities = {};
-
-    $http.get('/api/1/facets').success(function(data) {
-        $scope.facets = data.results;
-        angular.forEach(data.results, function(facet) {
-            loadFacetEntities(facet.key);
-        });
     });
 
-    function loadFacetEntities(facet_name) {
-        identity.session(function(ident) {
-            $http.get('/api/1/users/' + ident.user.id + '/entities?facet='+facet_name)
-            .success(function(data) {
-                $scope.entities[facet_name] = data.results;
-            });
-        });
-    }
-
-    $scope.create = function(facet) {
-        data = {facet: facet, text: $scope._new[facet]};
-        $scope._new[facet] = '';
-        console.log(data);
-        $http.post('/api/1/entities', data).success(function(data) {
-            $scope.entities[facet].push(data);
-        });
-    };
-
-    $scope.remove = function(facet, id) {
-        $http({method: 'delete', url: '/api/1/entities/' + id}).error(function(data, status) {
-            if (status===410) {
-                var entities = [];
-                angular.forEach($scope.entities[facet], function(entity) {
-                    console.log(entity);
-                    if (entity.id!==id) entities.push(entity);
-                });
-                $scope.entities[facet] = entities;
-            }
-        });
-    };
 }
