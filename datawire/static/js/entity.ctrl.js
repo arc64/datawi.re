a865d60310abbc8b006864c7a1594875f60ec9cf
Friedrich Lindenberg <friedrich@pudo.org>
2013-05-29 10:08:55 +0200

    Break out a few more JS bits.

---
+++ datawire/static/js/entity.ctrl.js
@@ -0,0 +1,43 @@
+
+function EntityCtrl($scope, $routeParams, $http, identity) {
+    $scope._new = {};
+    $scope.entities = {};
+
+    $http.get('/api/1/facets').success(function(data) {
+        $scope.facets = data.results;
+        angular.forEach(data.results, function(facet) {
+            loadFacetEntities(facet.key);
+        });
+    });
+
+    function loadFacetEntities(facet_name) {
+        identity.session(function(ident) {
+            $http.get('/api/1/users/' + ident.user.id + '/entities?facet='+facet_name)
+            .success(function(data) {
+                $scope.entities[facet_name] = data.results;
+            });
+        });
+    }
+
+    $scope.create = function(facet) {
+        data = {facet: facet, text: $scope._new[facet]};
+        $scope._new[facet] = '';
+        console.log(data);
+        $http.post('/api/1/entities', data).success(function(data) {
+            $scope.entities[facet].push(data);
+        });
+    };
+
+    $scope.remove = function(facet, id) {
+        $http({method: 'delete', url: '/api/1/entities/' + id}).error(function(data, status) {
+            if (status===410) {
+                var entities = [];
+                angular.forEach($scope.entities[facet], function(entity) {
+                    console.log(entity);
+                    if (entity.id!==id) entities.push(entity);
+                });
+                $scope.entities[facet] = entities;
+            }
+        });
+    };
+}
---
+++ datawire/static/js/services.svc.js
@@ -0,0 +1,33 @@
+
+datawire.factory('services', function($q, $http) {
+    var serviceDfds = {};
+
+    function getService(name, callback) {
+        if(!serviceDfds[name]) {
+            serviceDfds[name] = $http.get('/api/1/services/' + name);
+        }
+        serviceDfds[name].success(callback);
+        return serviceDfds[name];
+    }
+
+    function getFrame(ref) {
+        $http.get(ref.store_uri).success(function(frame) {
+            getService(frame.service, function(service) {
+                ref.event = _.find(service.events, function(e) {
+                    return e.key == frame.event;
+                });
+                if(ref.event.tmpl===undefined) {
+                    ref.event.tmpl = Handlebars.compile(ref.event.template);
+                }
+                ref.service = service;
+                ref.data = frame.data;
+                ref.message = ref.event.tmpl(frame.data);
+            });
+        });
+    }
+
+    return {
+        getService: getService,
+        getFrame: getFrame
+    };
+});
